23f0e2824caf0fca598158f9d69cc720cad1eadc
zephyrus21 <pandey.piyus@gmail.com>
2022-06-27 12:42:34 +0530

    addded title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
 import Starter from "@components/Starter";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
     <div className='flex flex-col justify-center items-center min-h-screen font-primary'>
       {/* Delete the Starter Component and remove <Starter /> to get started! */}
+      <Head>
+        <title>Next.js, TypeScript, Tailwind Starter Template</title>
+      </Head>
       <Starter />
     </div>
   );
